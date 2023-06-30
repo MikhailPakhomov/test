0e6078bee5b2e45a1879cc4234003833eb364747
Михаил Пахомов <mihail93@mail.ru>
2023-07-01 01:16:42 +0300

    fix: close mobile nav menu after ckick on the item

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const navButton = document.querySelector('.nav-mobile-button');
 const navMobile = document.querySelector('.nav-mobile');
-
+const navItem = document.querySelector('.nav-mobile-item')
 // const a = document.querySelector('.nav-mobile-active');
 // console.log(a);
 navButton.addEventListener('click', () => {
     navMobile.classList.toggle('nav-mobile-active');
     navButton.classList.toggle('nav-mobile-button-close');
-    
 });
 
+navItem.addEventListener('click', () => {
+    navMobile.classList.toggle('nav-mobile-active');
+})
 
 
